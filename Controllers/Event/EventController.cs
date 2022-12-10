b5f9e233b2ef968b0a689ab43bd497a367ffc01f
Margob29 <mbiret@ensc.fr>
2022-12-10 22:24:41 +0100

    EventDisplay

---
+++ Controllers/Event/EventController.cs
@@ -0,0 +1,14 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using projet_alex_margo.Models;
+
+namespace projet_alex_margo.Controllers;
+
+public class EventController : Controller
+{
+
+    public IActionResult Event()
+    {
+        return View();
+    }
+}
--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ builder.Services.AddDbContext<ENSCContext>();
 var app = builder.Build();
 
 //TODO : mettre une condition pour que ça le fasse que qd c'est vide
-SeedData.InitBd();
+//SeedData.InitBd();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
