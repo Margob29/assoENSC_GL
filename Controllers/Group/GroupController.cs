84cc9daa8d975d6a149026ab94823f0533b121f6
Alex Audebert <audebert.alex33@gmail.com>
2022-12-27 16:04:53 +0100

    club index

---
+++ Controllers/Group/GroupController.cs
@@ -0,0 +1,37 @@
+using Microsoft.AspNetCore.Mvc;
+using ENSC.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace ENSC.Controllers;
+
+public class GroupController : Controller
+{
+    private readonly ENSCContext _context;
+    public GroupController(ENSCContext context)
+    {
+        _context = context;
+    }
+
+    //GET all the groups
+    public async Task<IActionResult> Index()
+    {
+        var groups = await _context.Groups.Include(s => s.Events).ToListAsync();
+        return View(groups);
+    }
+
+    //GET an group with his id
+    public async Task<IActionResult> Details(int? id)
+    {
+        if (id == null)
+        {
+            return NotFound();
+        }
+        var e = await _context.Groups.Include(s => s.Events).SingleOrDefaultAsync(s => s.Id == id);
+        if (e == null)
+        {
+            return NotFound();
+        }
+        return View(e);
+    }
+
+}
--- Models/Group.cs
+++ Models/Group.cs
@@ -1,10 +1,17 @@
+using System.ComponentModel.DataAnnotations;
 namespace ENSC.Models;
 
 public class Group
 {
     public int Id { get; set; }
     public string Name { get; set; } = null!;
+
+    [Display(Name = "Nombre de membres")]
     public int? NbMembers { get; set; }
+
     public List<Event>? Events { get; set; }
-    public Student President { get; set; } = null!;
+
+    [Display(Name = "Président")]
+    public Student President { get; set; } = null!; // TODO ça devrait pas être un membre ?
+
 }
